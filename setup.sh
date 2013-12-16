f5720ab8ba7700b111184ecc2495d7c63ab3c669
Shaun Janssens <info@madebyshaun.be>
2013-12-16 13:26:43 +0100

    Automatisch installeren zonder opgave laatste versies

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-read -p "Wat is de laaste nieuwe versie van Webmin (www.webmin.com)? Vb: 1.660" webminversion
-echo "Webmin installeren voor systeem beheer en monitoring"
+webminversion="1.660"
+owncloudversion="6.0.0a"
+
+echo "Welkom bij de Raspberry Pi NAS installer!"
+echo "Volgende onderdelen zullen worden geïnstalleerd:"
+echo "- Webmin"
+echo "- Apache"
+echo "- php"
+echo "- MySQL"
+echo "- OwnCloud 6"
+echo "- Samba"
+echo " "
+
+echo "Webmin installeren"
 wget http://garr.dl.sourceforge.net/project/webadmin/webmin/${webminversion}/webmin_${webminversion}_all.deb
 dpkg -i webmin_${webminversion}_all.deb
 apt-get --force-yes install -f
 
-read -p "Wat is de laatste nieuwe versie van OwnCloud (www.owncloud.org)? Vb: 6.0.0a" owncloudversion
 echo "Webserver, php, database en OwnCloud installeren"
 apt-get --force-yes install apache2 php5 php5-gd php-xml-parser php5-intl
 apt-get --force-yes install php5-sqlite php5-mysql smbclient curl libcurl3 php5-curl
@@ -16,6 +27,7 @@ tar -xjf /var/www/owncloud-${owncloudversion}.tar.bz2
 
 echo "Samba installeren"
 apt-get --force-yes install samba
+
 echo "Server configureren"
 useradd nas
 passwd nas
@@ -23,5 +35,7 @@ usermod -aG www-data nas
 smbpasswd -a nas
 chmod 774 /var/www/owncloud/data/nas/files
 rm /etc/samba/smb.conf
-cp ./smb.conf /etc/samba
+wget https://raw.github.com/shaunjanssens/rpi-raid-owncloud-nas/master/smb.conf
+cp smb.conf /etc/samba
 service samba restart
+
