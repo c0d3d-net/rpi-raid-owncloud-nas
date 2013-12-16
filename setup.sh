8a91ced5a406a2fd66cc16cb57acac65376756f9
Shaun Janssens <info@madebyshaun.be>
2013-12-16 12:43:15 +0100

    Vertaling en RAID verwijderen

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-read -p "Geben sie die die Identifikationszahl der aktuellsten Webmin Version ein z.B 1.650:" webminversion
-echo "Installation von Webmin zur Systemüberwachung und der RAID konfiguration"
+read -p "Wat is de laaste nieuwe versie van Webmin (www.webmin.com)? Vb: 1.660" webminversion
+echo "Webmin installeren voor systeem beheer en monitoring"
 wget http://garr.dl.sourceforge.net/project/webadmin/webmin/${webminversion}/webmin_${webminversion}_all.deb
 dpkg -i webmin_${webminversion}_all.deb
 apt-get --force-yes install -f
 
-read -p "Geben sie die die Identifikationszahl der aktuellsten Owncloud Version ein z.B 5.0.12:" owncloudversion
-echo "Installation von Webserverkomponenten und OWNCLOUD"
+read -p "Wat is de laatste nieuwe versie van OwnCloud (www.owncloud.org)? Vb: 6.0.0a" owncloudversion
+echo "Webserver, php, database en OwnCloud installeren"
 apt-get --force-yes install apache2 php5 php5-gd php-xml-parser php5-intl
 apt-get --force-yes install php5-sqlite php5-mysql smbclient curl libcurl3 php5-curl
 apt-get --force-yes install mysql-server mysql-client
@@ -14,9 +14,9 @@ cd /var/www
 wget http://download.owncloud.org/community/owncloud-${owncloudversion}.tar.bz2
 tar -xjf /var/www/owncloud-${owncloudversion}.tar.bz2
 
-echo "Samba installation"
+echo "Samba installeren"
 apt-get --force-yes install samba
-echo "Benutzervorbereitung für den SAMBA gebrauch"
+echo "Server configureren"
 useradd nas
 passwd nas
 usermod -aG www-data nas
@@ -25,6 +25,3 @@ chmod 774 /var/www/owncloud/data/nas/files
 rm /etc/samba/smb.conf
 cp ./smb.conf /etc/samba
 service samba restart
-
-echo "RAID System Installation für spätere Implementation"
-apt-get --force-yes install mdadm
