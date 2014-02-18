8352640e5d4535bcddeb3a0cdff02af884160452
Piere Wöhl <woehlpiere@googlemail.com>
2014-02-18 15:53:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 webminversion="1.660"
 owncloudversion="6.0.0a"
 
-echo "Welkom bij de Raspberry Pi NAS installer!"
-echo "Volgende onderdelen zullen worden geïnstalleerd:"
+echo "Welcome at the Raspberry Pi NAS Installer!"
+echo "This Packages will be installed:"
 echo "- Webmin"
 echo "- Apache"
 echo "- php"
@@ -12,12 +12,12 @@ echo "- OwnCloud 6"
 echo "- Samba"
 echo " "
 
-echo "Webmin installeren"
+echo "Install Webmin"
 wget http://garr.dl.sourceforge.net/project/webadmin/webmin/${webminversion}/webmin_${webminversion}_all.deb
 dpkg -i webmin_${webminversion}_all.deb
 apt-get --force-yes install -f
 
-echo "Webserver, php, database en OwnCloud installeren"
+echo "Webserver, php, database and OwnCloud Install"
 apt-get --force-yes install apache2 php5 php5-gd php-xml-parser php5-intl
 apt-get --force-yes install php5-sqlite php5-mysql smbclient curl libcurl3 php5-curl
 apt-get --force-yes install mysql-server mysql-client
@@ -25,10 +25,10 @@ cd /var/www
 wget http://download.owncloud.org/community/owncloud-${owncloudversion}.tar.bz2
 tar -xjf owncloud-${owncloudversion}.tar.bz2
 
-echo "Samba installeren"
+echo "Samba Install"
 apt-get --force-yes install samba
 
-echo "Server configureren"
+echo "Server config"
 useradd nas
 passwd nas
 usermod -aG www-data nas
